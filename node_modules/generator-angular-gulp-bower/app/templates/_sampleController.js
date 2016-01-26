666feef3731513c94350980ca85d89baab4f5396
kesavasreeram <c.kesava.talabattula@uptake.com>
2016-01-26 15:13:03 -0600

    remove node_modules

--- node_modules/generator-angular-gulp-bower/app/templates/_sampleController.js
+++
@@ -1,9 +0,0 @@
-/*
- * Dashboard controller
- */
-'use strict';
-var controllerModule = angular.module('myApp.controller', []);
-
-controllerModule.controller('sampleCtrl', ['$scope', function($scope) {
-        
-    }]);
