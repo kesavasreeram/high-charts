fd14c71279500770009ff9c95d015ba2839d07ac
Kesava Sreeram Talabattula <kesavasreeram@gmail.com>
2016-01-26 14:58:15 -0600

    init: make genric changes to root project folder

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,66 @@
+/**
+ * Created by ktalabattula on 1/26/16.
+ */
+
 'use strict';
 
+// This checks if gulp is installed for this project or not
+// looks for 'gulp' folder. Right now it is available in node_modules
 var gulp = require('gulp');
 
-require('require-dir')('./gulp');
+// Sass requirement for stylesheets
+var sass = require('gulp-sass');
 
-gulp.task('default', ['clean'], function() {
-    gulp.start('build');
+// concatinating the compiled files
+var concat = require('gulp-concat');
+
+// When you give 'gulp <task name>' command in terminal this task will be invoked
+// If no task name is specified it will execute the 'default' task
+// Syntax gulp.task('<task name>',['<dependency tasks which are executed before
+// executing this task>'], '<function: what needs to be done while executing this task>')
+
+gulp.task('default', ['styles'], function() {
+    console.log('This is default task');
 });
+
+gulp.task('styles', function(){
+    // gulp.src will gather all/specific file
+    // .pipe will compile sass files using gulp-sass and pipe them down
+    // concat will concatinate all the compiled styles into one file
+    // dest will output the 'generated file' into the specified location.
+    gulp.src(
+        ['./main/resources/stylesheets/styles.scss'])
+        .pipe(sass(
+            // If you need to include external files like bootstrap related, foundation related
+            // you can specify them here so they will be compiled and added
+            // and you dont have to add the include statements in index.html or any other files
+            // syntax is as below:
+            // [includePaths:['<path>']]  <-- in case of components installed using bower specify
+            // that path instead
+        ))
+        .pipe(concat('app.css'))
+        .pipe(gulp.dest('./www/stylesheets'));
+
+    gulp.src(
+        ['./main/index.html'])
+        .pipe(gulp.dest('./www/'));
+
+});
+
+// in case you dont want to do a gulp command to pick up your css changes
+// you can create another task called 'watch' which will update any files that
+// are changed
+
+gulp.task('watch', function(){
+    // When ever there is a change in the path specified in 'watch', it will run the
+    // tasks specified, in this case it is 'styles' task.
+    gulp.watch('./main/resources/stylesheets/**/*.scss',['styles']);
+})
+
+
+/***************** Scripts **********************/
+gulp.task('scripts',function(){
+    gulp.src(['./bower_components/angular/angular.js'])
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('./www/scripts'));
+})
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # high-charts
 POC for highcharts
+
+# Changes that need to be made when you are using this base code
+1. Change the description and project name in bower.json and package.json
+2. Only Angular and Gulp are added as dependencies.
+3. If you need any other module dependencies add them using respective settings: bower or npm
+
+# Commands used to create this base project
+1. npm install -g bower
+2. bower init
+3. bower install angular --save
+4. npm init
+5. npm install gulp --save-dev
+6. npm install gulp-sass --save-dev
